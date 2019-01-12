34ea8823f7c2b9906bded359159683e3c3141daf
bhgv <bhgv.empire@gmail.com>
2019-01-12 05:32:38 +0300

    compression algo was improved. now it can find and encode identical digit sequences

--- compress.py
+++ compress.py
@@ -55,10 +55,6 @@ f.close()
 
 r = xbm_rd(txt)
 
-#if r:
-#    print "name=%s, w=%d, h=%d\n" % (r['name'], r['w'], r['h'], )
-#    print "bits:\n%s\n" % (r['bits'], )
-
 w = r['w']
 h = r['h']
 nm = r['name']
@@ -73,38 +69,75 @@ n0 = 0
 n1 = 0
 nx = 0
 
+a_ttl  = []
+a_posx = []
+
+
+def encode_x_dobles():
+    global a_ttl, a_posx
+
+    i = 0
+    i_max = len(a_posx)
+    while i < i_max:
+        j = a_posx[i]
+        ln = a_ttl[j]
+
+        i_from = i-64
+        if i_from < 0:
+            i_from = 0
+
+        for k in xrange(i_from, i):
+            k_a_ttl = a_posx[k]
+
+            if ln == a_ttl[k_a_ttl]:
+                a_ttl[j] = [0xC0 + (k-i_from)]
+                del a_posx[i]
+                i = i - 1
+                i_max = i_max - 1
+                break
+        i = i + 1
+
+
+
 def _store_count(f, pref, n):
+    global a_ttl, a_posx
+    pos_ttl = -1
     while True:
         if n >= 0x40:
             cnt = 0x40 - 1
         else:
             cnt = n
-        f.write("0x%X, " % ( pref + cnt, ))
+
+#        f.write("0x%X, " % ( pref + cnt, ))
+        a_ttl.append([pref + cnt,])
+        pos_ttl = len(a_ttl) - 1
+        if pref == 0x80:
+            a_posx.append(pos_ttl)
+
         n = n - cnt
         if n <= 0:
             break
+    return pos_ttl
 
 def _if_store_0(f):
     global n0
     if n0 > 0:
-#        print "n0 = %d\n" % (n0, )
         _store_count(f, 0x00, n0)
     n0 = 0
 
 def _if_store_1(f):
     global n1
     if n1 > 0:
-#        print "n1 = %d\n" % (n1, )
         _store_count(f, 0x40, n1)
     n1 = 0
 
 def _if_store_x(f, bits, i):
     global nx
     if nx > 0:
-#        print "nx = %d\n" % (nx, )
-        _store_count(f, 0x80, nx)
+        pos = _store_count(f, 0x80, nx)
         for j in bits[i - nx:i]:
-            f.write("0x%X, " % ( j, ))
+            a_ttl[pos].append(j)
+#            f.write("0x%X, " % ( j, ))
     nx = 0
 
 
@@ -112,25 +145,32 @@ for i in xrange(len(bits)):
     c = bits[i]
     if c == 0:
         n0 = n0 + 1
-#       print "n0 = %d\n" % (n0, )
         _if_store_1(f)
         _if_store_x(f, bits, i)
     elif c == 0xff:
         n1 = n1 + 1
-#       print "n1 = %d\n" % (n1, )
         _if_store_0(f)
         _if_store_x(f, bits, i)
     else:
         if nx == 0x40 - 1:
             _if_store_x(f, bits, i)
         nx = nx + 1
-#       print "nx = %d\n" % (nx, )
         _if_store_0(f)
         _if_store_1(f)
+
 _if_store_0(f)
 _if_store_1(f)
 _if_store_x(f, bits, i)
 
+encode_x_dobles()
+
+for ln in a_ttl:
+#    f.write("    "),
+    for i in ln:
+        f.write("0x%X, " % ( i, ))
+#    f.write("\n"),
+
+#f.write("    ")
 f.write("0x0,\n")
 
 f.write("};\n")
--- example/draw_compressed_char.c
+++ example/draw_compressed_char.c
@@ -1,59 +1,104 @@
 
+#include <stdio.h>
+#include <stdint.h>
+
+#include "z_049.xbm"
+
+
+static uint16_t xp, yp;
+
+uint16_t yo = 0;
 
 static void __aux_Paint_DrawByteXBM(uint16_t x, uint16_t y, uint8_t b)
 {
     uint8_t i;
     uint8_t clr;
 
+        if(y != yo){
+            printf("\ny=%d) ", y);
+            yo = y;
+        }
+
     for(i = 8; i > 0; i--){
         clr = (b & 1) == 0;
-        Paint_SetPixel(x, y, clr);
+        //Paint_SetPixel(x, y, clr);
+        printf(clr ? "0" : "_");
+
         x++;
         b >>= 1;
     }
 }
 
 
-void Paint_DrawCompressedXBM(uint16_t x, uint16_t y, uint16_t w, uint16_t h,
-                         const uint8_t* xbm_buffer)
+static uint16_t __aux_Paint_DecompressDrawByteXBM(uint16_t x, uint16_t y,
+                                              uint16_t w, uint16_t h,
+                                              const uint8_t* xbm_buffer,
+                                              uint16_t i,
+                                              uint16_t a_xp[], uint8_t *a_xp_n)
 {
-    uint16_t i, j;
-    uint8_t b, n;
-    uint16_t xp, yp;
+    uint16_t j;
+    uint8_t b = xbm_buffer[i];
+    uint8_t n = b & (0x40 - 1);
 
-    xp = 0; yp = 0;
-    for(i = 0; xbm_buffer[i] != 0; ){
-        b = xbm_buffer[i];
-        i++;
-        n = b & (0x40 - 1);
+    i++;
 
-        switch(b & 0xC0){
-        case 0x00:
-            for(j = 0; j < n; j++){
-                __aux_Paint_DrawByteXBM(x + xp, y + yp, 0);
-                xp += 8;
-                if(xp >= w){
-                    xp = 0;
-                    yp++;
-                }
+    switch(b & 0xC0){
+    case 0x00:
+        for(j = 0; j < n; j++){
+            __aux_Paint_DrawByteXBM(x + xp, y + yp, 0);
+            xp += 8;
+            if(xp >= w){
+                xp = 0;
+                yp++;
             }
-            break;
+        }
+        break;
 
-        case 0x40:
-            for(j = 0; j < n; j++){
-                __aux_Paint_DrawByteXBM(x + xp, y + yp, 0xFF);
-                xp += 8;
-                if(xp >= w){
-                    xp = 0;
-                    yp++;
-                }
+    case 0x40:
+        for(j = 0; j < n; j++){
+            __aux_Paint_DrawByteXBM(x + xp, y + yp, 0xFF);
+            xp += 8;
+            if(xp >= w){
+                xp = 0;
+                yp++;
             }
-            break;
+        }
+        break;
 
-        case 0x80:
+    case 0x80:
+        if(*a_xp_n == 64){
+            uint8_t idx;
+
+            for(idx = 0; idx < 63; idx++)
+                a_xp[idx] = a_xp[idx + 1];
+            a_xp[63] = i - 1;
+        }else{
+            a_xp[*a_xp_n] = i - 1;
+            (*a_xp_n)++;
+        }
+
+        for(j = 0; j < n; j++){
+            b = xbm_buffer[i];
+            i++;
+            __aux_Paint_DrawByteXBM(x + xp, y + yp, b);
+            xp += 8;
+            if(xp >= w){
+                xp = 0;
+                yp++;
+            }
+        }
+        break;
+
+    case 0xC0:
+        if(n <= *a_xp_n){
+            uint16_t i_tmp = a_xp[n];
+
+            b = xbm_buffer[i_tmp];
+            i_tmp++;
+            n = b & (0x40 - 1);
             for(j = 0; j < n; j++){
-                b = xbm_buffer[i];
-                i++;
+                b = xbm_buffer[i_tmp];
+                i_tmp++;
                 __aux_Paint_DrawByteXBM(x + xp, y + yp, b);
                 xp += 8;
                 if(xp >= w){
@@ -61,10 +106,30 @@ void Paint_DrawCompressedXBM(uint16_t x, uint16_t y, uint16_t w, uint16_t h,
                     yp++;
                 }
             }
-            break;
-
         }
-        if(yp >= h)
-            break;
+        break;
+
     }
+    return i;
+}
+
+
+void Paint_DrawCompressedXBM(uint16_t x, uint16_t y, uint16_t w, uint16_t h,
+                         const uint8_t* xbm_buffer)
+{
+    uint16_t i;
+
+    uint16_t a_xp[64];
+    uint8_t  a_xp_n = 0;
+
+    xp = 0; yp = 0;
+    for(i = 0; xbm_buffer[i] != (uint8_t)0 && yp < h; ){
+        i = __aux_Paint_DecompressDrawByteXBM(x, y, w, h, xbm_buffer, i, a_xp, &a_xp_n);
+    }
+}
+
+
+int
+main(){
+    Paint_DrawCompressedXBM(0, 0, z_049_width, z_049_height, z_049_bits);
 }
